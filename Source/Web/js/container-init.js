36622dd3625ac29865bb939cb710a55aefeec46d
Tania Shatilova <tatashat@gmail.com>
2016-02-21 09:56:44 +0300

    - useless code

--- Source/Web/js/container-init.js
+++ Source/Web/js/container-init.js
@@ -9,18 +9,5 @@ function containerDim(selector, dim) {
 
 $(function() {
     $('a[href*="http"]').attr('target', '_blank');
-    $('.nav a[href="'+document.location.pathname+'"]').parent('li').attr('class', 'active');
-
-    $('.share a').click(function(e) {
-        e.preventDefault();
-        var b = $(this);
-        var action = $.trim(b.find('i').attr('class').replace('icon-', ''));
-        _gaq.push(['_trackEvent', 'Sharemenu', action]);
-        window.open(b.attr('href'));
-    });
-
-    $('.showhelp').click(function(e) {
-        e.preventDefault();
-        $('#help').modal();
-    });
+    $('.nav a[href="'+document.location.pathname+'"]').parent('li').attr('class', 'active');    
 });
