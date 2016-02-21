8f841eec615a317822d940fda9e0ec90a41f737c
GSerjo <sedot8@gmail.com>
2016-02-21 02:16:34 -0800

    + Courses initial

--- Source/DataGenerator/DataRepository.cs
+++ Source/DataGenerator/DataRepository.cs
@@ -188,6 +188,10 @@ namespace DataGenerator
                 {
                     result.Add($"{_libNode.Id}; {item.Id}");
                 }
+                foreach (var item in _node.PluralsightCourses)
+                {
+                    result.Add($"{_libNode.Id}; {item.Id}");
+                }
                 return result;
             }
         }
@@ -218,7 +222,36 @@ namespace DataGenerator
                 var items1 = new string(';', RepositoryInfoEntity.Captions().Count - 1);
                 var items2 = new string(';', CourseEntity.Captions().Count - 1);
                 var items3 = new string(';', UserEntity.Captions().Count - 1);
-                return $"{items1};{items2};{items3};{Id}; {_node.Tag}";
+                return $"{items1};{items2};{items3};{Id}; {_node.Tag} Libs";
+            }
+        }
+
+        private sealed class CourseNode
+        {
+            private const string Suffix = "CourseNode";
+            private readonly NodeRow _node;
+
+            public CourseNode(NodeRow node)
+            {
+                _node = node;
+                Id = $"{_node.Tag}{Suffix}";
+            }
+
+            public string Id { get; }
+
+            public static List<string> Captions()
+            {
+                var items = new[] { "Id", "Label" };
+                List<string> result = items.Select(x => $"{x}{Suffix}").ToList();
+                return result;
+            }
+
+            public override string ToString()
+            {
+                var items1 = new string(';', RepositoryInfoEntity.Captions().Count - 1);
+                var items2 = new string(';', CourseEntity.Captions().Count - 1);
+                var items3 = new string(';', UserEntity.Captions().Count - 1);
+                return $"{items1};{items2};{items3};{Id}; {_node.Tag} Courses";
             }
         }
     }
