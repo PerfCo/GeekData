851183f88da8f52ade87085208474c8c4ca6360b
Tata-sh <tatashat@gmail.com>
2016-02-25 23:44:39 +0300

    * content of gexf-vis-init.js moved to file event-handler.js

--- Source/Web/js/event-handler.js
+++ Source/Web/js/event-handler.js
@@ -35,11 +35,44 @@
         }
     }
 
-    win.addEventListener("load", function() {
+    function initAncors() {
         var ancors = getAncors();
         for (var a in ancors) {
             ancors[a].addEventListener("click", onUrlClick)
         }
+    }
+
+    function initVisgexf() {
+        var props = {
+            drawing: {
+                defaultLabelColor: '#fff',
+                defaultLabelSize: 12,
+                defaultLabelBGColor: '#fff',
+                defaultLabelHoverColor: '#000',
+                labelThreshold: 4, // minimum size a node has to have to have its label being displayed
+                defaultEdgeType: 'curve'
+            },
+            graph: {
+                minNodeSize: .5,
+                maxNodeSize: 25,
+                minEdgeSize: 1,
+                maxEdgeSize: 1
+            },
+            forceLabel: 1,
+            type: 'directed'
+        }
+
+        App.visgexf.init('sig', gexf, props, function() {
+            var filterid = 'paradigms';
+            var filters = App.visgexf.getFilters([filterid]);
+        });
+    }
+
+    $(function(){
+        initVisgexf();
     });
 
+    $(window).load(function () {
+        initAncors();
+    });
 }(window, document));
