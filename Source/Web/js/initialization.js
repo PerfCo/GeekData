9263ced90881d9dd1f3ccfadbcb1af5e19a596dd
Tata-sh <tatashat@gmail.com>
2016-02-27 11:10:07 +0300

    - useless code

--- Source/Web/js/initialization.js
+++ Source/Web/js/initialization.js
@@ -1,47 +1,4 @@
 (function(win, doc) {
-
-    function getAncors() {
-        var links = doc.getElementsByTagName("a");
-        var ancors = [];
-        for (var i = 0, length = links.length; i < length; ++i) {
-            var href = links[i];
-            if (href.host != win.location.host && href.className != "github-corner") {
-                ancors.push(href);
-            }
-        }
-        return ancors;
-    }
-
-    function onUrlClick(ancor) {
-        var url = ancor.srcElement || ancor.target;
-        while (url && (typeof url.tagName == "undefined" || url.tagName.toLowerCase() != "a" || !url.href)) {
-            url = url.parentNode
-        }
-        if (!url || !url.href) {
-            return
-        }
-        if (win.ga) {
-            ga("send", "event", "Outbound link", "Click", url.href)
-        }
-        if (!url.target || url.target.match(/^_(self|parent|top)$/i)) {
-            setTimeout(function() {
-                ancor.location.href = url.href
-            }, 150);
-            if (ancor.preventDefault) {
-                ancor.preventDefault()
-            } else {
-                ancor.returnValue = false
-            }
-        }
-    }
-
-    function initAncors() {
-        var ancors = getAncors();
-        for (var a in ancors) {
-            ancors[a].addEventListener("click", onUrlClick)
-        }
-    }
-
     function initVisualization() {
         var options = {
 
@@ -68,17 +25,10 @@
             }
         };
 
-        App.dataVisualization.init(options, function() {
-            var filterid = 'paradigms';
-            var filters = App.dataVisualization.getFilters([filterid]);
-        });
+        App.dataVisualization.init(options);
     }
 
     $(function(){
         initVisualization();
-    });
-
-    $(window).load(function () {
-        initAncors();
     });
 }(window, document));
